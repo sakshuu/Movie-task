ebf52f1d9edc58973580602f169d7e41eda215cc
sakshi jadhav <sakshisjadhav.120@gmail.com>
2023-05-04 20:53:15 +0530

    initial commit

---
+++ src/pages/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MovieDetails, MySwiper } from '../components'
+
+const Details = () => {
+  return <>  
+<MovieDetails/>
+{/* <Comments/> */}
+<MySwiper/>
+  </>
+}
+
+export default Details
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MoviesShow, MyCarousel } from '../components'
+
+const Home = () => {
+  return <>
+  <MyCarousel/>
+  <MoviesShow/>
+ 
+  </>
+}
+
+export default Home
